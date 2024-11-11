e198cf575d6a5e9a95243cadd02fb0b953d1ee4c
analy.kammer <110103286+anajkerndkrj@users.noreply.github.com>
2024-11-11 08:10:02 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const cixaPrinccipal = document.querySelector(".caixa__principal");
+const caixaPerguntas = document.querySelector(".caixa__perguntas");
+const caixaAlternativas = document.querySelector(".caixa__alternativas");
+const caixaResultados = document.querySelector
